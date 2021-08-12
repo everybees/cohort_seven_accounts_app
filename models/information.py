3e64d1ebe9bb9f8673a20b322d7cbc772b5d26a6
Kelvin Okoro <okorokelvinemoakpooke@yahoo.com>
2021-08-12 11:57:28 +0100

    added contributors file

--- models/information.py
+++ models/information.py
@@ -1,4 +1,4 @@
-class UserInformation():
+class UserInformation:
     def __init__(self, first_name, last_name, phone_number, password):
         self.first_name = first_name
         self.last_name = last_name
